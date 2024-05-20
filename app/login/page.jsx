8a26c17b69b52444f4702dd0b3797dc58db678c7
arnold-aswan <fredyaswan@gmail.com>
2024-05-20 18:25:12 +0300

    login page

---
+++ app/login/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Login = () => {
+  return (
+    <div className="flex items-center justify-center">
+      <div className="bg-red-500 ">Sign in with Google</div>
+    </div>
+  );
+};
+
+export default Login;
