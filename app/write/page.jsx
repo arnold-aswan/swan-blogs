e46ade8ab16c03e187378c717d678d7b28d65c21
arnold-aswan <fredyaswan@gmail.com>
2024-05-21 13:33:58 +0300

    added publish button

--- app/write/page.jsx
+++ app/write/page.jsx
@@ -23,13 +23,16 @@ const WritePage = () => {
   };
   return (
     <div>
-      WritePage
       <ReactQuill
         theme="snow"
         modules={modules}
         value={value}
         onChange={setValue}
+        placeholder="Tell your story..."
       />
+      <button className="mt-8 py-2 px-8 rounded-lg font-semibold bg-dark-soft-bg text-dark-text dark:bg-light-soft-bg dark:text-light-soft-text hover:drop-shadow-xl hover:scale-110 transition ease-out delay-250">
+        Publish
+      </button>
     </div>
   );
 };
